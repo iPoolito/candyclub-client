55a119b4a78556c73ea4fcdf8f4b75ad85eddf29
Pool Ortega <poolortegas@gmail.com>
2021-10-19 20:56:53 -0500

    Home Change

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,7 @@
 import { useEffect, useState } from 'react'
 import { useKeenSlider } from 'keen-slider/react'
 import PRODUCTS_API from '../api/products'
-import { Flex, Image, Heading, VStack, Box, Text, Button } from '@chakra-ui/react'
+import { Flex, Image, Heading, VStack, Box, Text, Button, SimpleGrid } from '@chakra-ui/react'
 import 'keen-slider/keen-slider.min.css'
 import { Link } from 'react-router-dom'
 import { FaFacebook, FaInstagram, FaTiktok } from 'react-icons/fa'
@@ -53,7 +53,18 @@ export default function Home() {
   }
 
   return (
-    <>
+    <Flex
+      as="main"
+      w="100%"
+      inH="calc(100vh - 80px)"
+      backgroundColor=" #fbd3d4 "
+      backgroundRepeat="no-repeat"
+      align="center"
+      justify="space-between"
+      p="24px"
+      flexDirection="column"
+      color="black"
+    >
       <div className="navigation-wrapper">
         <div ref={sliderRef} className="keen-slider">
           {listOfProducts.map((product, i) => (
@@ -88,90 +99,98 @@ export default function Home() {
           })}
         </div>
       )}
-      <VStack spacing={8} mt={16} mb={16}>
-        <Box h="40px" as="h2">
-          <Heading>Sobre Nosotros</Heading>
-        </Box>
-        <Box h="40px" as="p" px={80}>
-          <Text fontSize="xl">
-            Somos una empresa comercializadora de alimentos de excelente calidad, comprometida en deleitar con cariño y
-            calidez a la comunidad en que nos desenvolvemos. Dedicamos el tiempo necesario a las necesidades de cada
-            cliente. Contamos con Dulces y Cereales importados con precios accesibles. Así conseguimos que nuestros
-            productos sean una delicia para los ojos y un capricho para el paladar.
-          </Text>
-        </Box>
-      </VStack>
-      <VStack spacing={8} mt={32}>
-        <Box h="40px" as="h2">
-          <Heading>Siguenos en nuestras redes sociales</Heading>
-        </Box>
-        <Flex h="40px" px={16} justifyContent="space-between" w="40%" align="center" mt={16}>
-          <Button colorScheme="facebook" leftIcon={<FaFacebook />}>
-            Facebook
-          </Button>
-          <Button colorScheme="pink" leftIcon={<FaInstagram />}>
-            Instagram
-          </Button>
-          <Button colorScheme="red" leftIcon={<FaTiktok />}>
-            Tiktok
-          </Button>
+      <SimpleGrid columns={[1, 1, 1, 1]} spacing={40}>
+        <Flex spacing={8} mt={8} mb={8} flexDirection="column" align="center" justifyContent="center" w="100%">
+          <Box h="40px" as="h2" mb={8}>
+            <Heading>Sobre Nosotros</Heading>
+          </Box>
+          <Box h="40px" as="p" w="56%">
+            <Text fontSize="xl">
+              Somos una empresa comercializadora de alimentos de excelente calidad, comprometida en deleitar con cariño
+              y calidez a la comunidad en que nos desenvolvemos. Dedicamos el tiempo necesario a las necesidades de cada
+              cliente. Contamos con Dulces y Cereales importados con precios accesibles. Así conseguimos que nuestros
+              productos sean una delicia para los ojos y un capricho para el paladar.
+            </Text>
+          </Box>
         </Flex>
-      </VStack>
-      <VStack spacing={8} mt={16}>
-        <Box h="40px" as="h2">
-          <Heading>Marcas </Heading>
-        </Box>
-        <Flex h="40px" px={4} flexWrap="wrap" justifyContent="space-between" w="50%" mb={16}>
-          <Image
-            borderRadius="full"
-            boxSize="150px"
-            src="https://upload.wikimedia.org/wikipedia/commons/0/05/Chips_ahoy_brandlogo.png"
-            alt="Chips Ahoy"
-          />
-          <Image
-            borderRadius="full"
-            boxSize="150px"
-            src="http://assets.stickpng.com/images/5ae22d4c33b73fa43b1a8939.png"
-            alt="Kit Kat"
-          />
-          <Image
-            borderRadius="full"
-            boxSize="150px"
-            src="https://contact.pepsico.com/files/capncrunch/brands/1543851225/capncrunch_logo4@2x.png"
-            alt="Capitan Crunch"
-          />
-          <Image
-            borderRadius="full"
-            boxSize="150px"
-            src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/24/Cheetos_logo.svg/1200px-Cheetos_logo.svg.png"
-            alt="Cheetos"
-          />
-          <Image
-            borderRadius="full"
-            boxSize="150px"
-            src="https://americanlicorice.com/new/wp-content/uploads/2021/04/rv_brand_logo.png"
-            alt="RedVines"
-          />
-          <Image
-            borderRadius="full"
-            boxSize="150px"
-            src="https://1000marcas.net/wp-content/uploads/2020/10/Oreo-logo.png"
-            alt="Oreo"
-          />
-          <Image
-            borderRadius="full"
-            boxSize="150px"
-            src="https://www.conagrafoodservice.com/sites/g/files/qyyrlu531/files/images/brands/Swiss-Miss-logo_0.png"
-            alt="SwissMiss"
-          />
-          <Image
-            borderRadius="full"
-            boxSize="150px"
-            src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Reese%27s_logo.svg/1200px-Reese%27s_logo.svg.png"
-            alt="Rees'es"
-          />
+        <Flex spacing={8} justifyContent="space-between" align="center" flexDirection="column">
+          <Box h="40px" as="h2">
+            <Heading>Siguenos en nuestras redes sociales</Heading>
+          </Box>
+          <Flex h="40px" px={16} justifyContent="space-between" w="40%" align="center" mt={16}>
+            <a href="https://www.facebook.com/candyclub.cuerna">
+              <Button colorScheme="facebook" leftIcon={<FaFacebook />}>
+                Facebook
+              </Button>
+            </a>
+            <a href="https://www.instagram.com/candyclub.cuerna/">
+              <Button colorScheme="pink" leftIcon={<FaInstagram />}>
+                Instagram
+              </Button>
+            </a>
+            <a href="https://www.tiktok.com/@candyclub.cuerna/">
+              <Button colorScheme="red" leftIcon={<FaTiktok />}>
+                Tiktok
+              </Button>
+            </a>
+          </Flex>
         </Flex>
-      </VStack>
-    </>
+        <Flex justifyContent="space-between" align="center" flexDirection="column">
+          <Box h="40px" as="h2" mb={8}>
+            <Heading>Marcas </Heading>
+          </Box>
+          <SimpleGrid columns={[1, 1, 2, 4]} spacing={10}>
+            <Image
+              borderRadius="full"
+              boxSize="150px"
+              src="https://upload.wikimedia.org/wikipedia/commons/0/05/Chips_ahoy_brandlogo.png"
+              alt="Chips Ahoy"
+            />
+            <Image
+              borderRadius="full"
+              boxSize="150px"
+              src="http://assets.stickpng.com/images/5ae22d4c33b73fa43b1a8939.png"
+              alt="Kit Kat"
+            />
+            <Image
+              borderRadius="full"
+              boxSize="150px"
+              src="https://contact.pepsico.com/files/capncrunch/brands/1543851225/capncrunch_logo4@2x.png"
+              alt="Capitan Crunch"
+            />
+            <Image
+              borderRadius="full"
+              boxSize="150px"
+              src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/24/Cheetos_logo.svg/1200px-Cheetos_logo.svg.png"
+              alt="Cheetos"
+            />
+            <Image
+              borderRadius="full"
+              boxSize="150px"
+              src="https://americanlicorice.com/new/wp-content/uploads/2021/04/rv_brand_logo.png"
+              alt="RedVines"
+            />
+            <Image
+              borderRadius="full"
+              boxSize="150px"
+              src="https://1000marcas.net/wp-content/uploads/2020/10/Oreo-logo.png"
+              alt="Oreo"
+            />
+            <Image
+              borderRadius="full"
+              boxSize="150px"
+              src="https://www.conagrafoodservice.com/sites/g/files/qyyrlu531/files/images/brands/Swiss-Miss-logo_0.png"
+              alt="SwissMiss"
+            />
+            <Image
+              borderRadius="full"
+              boxSize="150px"
+              src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Reese%27s_logo.svg/1200px-Reese%27s_logo.svg.png"
+              alt="Rees'es"
+            />
+          </SimpleGrid>
+        </Flex>
+      </SimpleGrid>
+    </Flex>
   )
 }
